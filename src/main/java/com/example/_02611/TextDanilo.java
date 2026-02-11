84cba763fa0c3d19f5e2af478479d5b59ece3924
Danilo m. <danilo.ladinom@uqvirtual.edu.co>
2026-02-11 13:04:52 -0500

    Agrego clase MensajeDanilo

--- src/main/java/com/example/_02611/TextDanilo.java
+++ src/main/java/com/example/_02611/TextDanilo.java
@@ -2,6 +2,6 @@ package com.example._02611;
 
 public class TextDanilo {
      static void main(String[] args) {
-        System.out.println("Hola, este es un mensaje creado por Danilo");
+        System.out.println("Hola, este es ul segundo commit creado por Danilo");
     }
 }
